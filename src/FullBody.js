65e80cf89609aa2aece6fa1e51689174f5f5547e
Colm Carew <colm.carew@over-c.com>
2020-07-22 16:38:47 +0100

    Initial Idea working

--- src/FullBody.js
+++ src/FullBody.js
@@ -1,12 +1,100 @@
 import React, { Component } from 'react';
 
 class FullBody extends Component {
+
+  constructor(props) {
+      super(props)
+      this.state = {
+        TC : 275,
+        USC : 0,
+        PC : 0,
+        GOV : 1516.67
+      }
+    }
+
+  setJanNet(janNetEvent) {
+    console.log(janNetEvent.target.value);
+      this.setState({
+        janNet : janNetEvent.target.value
+      });
+  }
+
+  setFebNet(febNetEvent) {
+    console.log(febNetEvent.target.value);
+      this.setState({
+        febNet : febNetEvent.target.value
+      });
+  }
+
+  setTC(tcEvent) {
+    console.log(tcEvent.target.value);
+      this.setState({
+        taxCredits : tcEvent.target.value
+      });
+  }
+
+  setPC(event) {
+    console.log(event.target.value);
+      this.setState({
+        PC : event.target.value
+      });
+  }
+
+  setGov(event) {
+    console.log(event.target.value);
+      this.setState({
+        GOV : event.target.value
+      });
+  }
+
+  setUSC(uSCEvent) {
+    console.log(uSCEvent.target.value);
+      this.setState({
+        USC : uSCEvent.target.value
+      });
+  }
+
+  calculateNet(){
+    const janNet = Number(this.state.janNet);
+    if(!janNet){
+        return 0;
+    }
+    console.log("Jan Net " + janNet);
+    let febNet = Number(this.state.febNet);
+    if(!febNet){
+      febNet = janNet;
+    }
+    console.log("Feb Net " + febNet);
+    const janNetAndFebNet = janNet + febNet;
+    console.log("Jan Net and Feb Net " + janNetAndFebNet);
+    const totalPay = ((Number(janNetAndFebNet)/9)*(52/12)) - Number(this.state.GOV);
+    console.log("Total Pay " + totalPay);
+    const totalGross = totalPay - Number(this.state.PC);
+    console.log("Total Gross " + totalGross);
+    const PAYE = Number(Number(totalGross)*0.2) - Number(this.state.TC);
+    console.log("PAYE " + PAYE);
+    const totalNet = totalGross - PAYE - Number(this.state.USC) + Number(this.state.GOV);
+    console.log("Total Net " + totalNet);
+    return totalNet;
+  }
+
   render() {
+    const calcNet = this.calculateNet();
+
     return (
         <div className="container-fluid">
            <div className="col-md-12">
              <div className="col-md-6">
                 <p>calculator</p>
+                <p>Jan Net : <input value={this.state.financialGoal} type="number" onChange={this.setJanNet.bind(this)}/></p>
+                <p>Feb Net : <input value={this.state.financialGoal} type="number" onChange={this.setFebNet.bind(this)}/></p>
+                <p>Tax Credits Per Month : <input value={this.state.financialGoal} type="number" onChange={this.setTC.bind(this)}/></p>
+                <p>Pension Contribution : <input value={this.state.financialGoal} type="number" onChange={this.setPC.bind(this)}/></p>
+                <p>Gov Subsidy : <input value={this.state.financialGoal} type="number" onChange={this.setGov.bind(this)}/></p>
+                <p>USC : <input value={this.state.financialGoal} type="number" onChange={this.setUSC.bind(this)}/></p>
+                <br/>
+                <br/>
+                <p>Net Net : <strong>€{calcNet}</strong></p>
              </div>
              <div className="col-md-6">
                 <p>Formula for Total Pay = ((Jan Net + Feb Net)/9 weeks)(52 weeks/12 months) - Gov Subsidy</p>
@@ -17,8 +105,8 @@ class FullBody extends Component {
                 <br/>
                 <p>Total Pay by company = (2X/9)(52/12) - 1516.67 = (26X/27) - 1516.67</p>
                 <p>Total Gross Pay = Total Pay by company - Pension Contribution</p>
-                <p>PAYE = 20% of Gross = 0.2((26X/27) - 1516.67 -PC)</p>
-                <p>Net Pay = Total Gross Pay - PAYE - USC + TC + Gov Subsidy</p>
+                <p>PAYE = 20% of Gross = 0.2((26X/27) - 1516.67 -PC) - TC</p>
+                <p>Net Pay = Total Gross Pay - PAYE - USC + Gov Subsidy</p>
                 <br/>
                 <br/>
                 <p>Net Pay = (26X/27) - 1516.67 - PC -(0.2((26X/27) - 1516.67 -PC)) - USC + TC + 1516.67</p>
